d7c5ee2210d1ae33cfcbab5cbd99591afee0e379
Mensur <m.bektic@appdevelopment.de>
2019-12-26 21:43:01 +0100

    Added head to single post.

--- src/app/posts/components/post.component.tsx
+++ src/app/posts/components/post.component.tsx
@@ -1,4 +1,5 @@
 import { Container } from 'reactstrap';
+import Head from 'next/head';
 
 import CustomLoader from '../../shared/loader/components/custom-loader.component';
 import { Post } from "../../../core/redux/posts/posts.interface";
@@ -11,27 +12,35 @@ interface PropsType {
 }
 
 const PostComponent = ({ post, isFetching }: PropsType) => (
-
-  <div className="d-flex flex-column flex-wrap single-post">
-    <img src="https://d3sgyrafn929g0.cloudfront.net/wp-content/uploads/sites/18/2016/05/27100558/cover-5-600x700.jpg" className="img-fluid" />
-    <Container>
-      {isFetching &&
-        <div className="d-flex flex-row flex-wrap justify-content-center">
-          <CustomLoader />
-        </div>
-      }
-      {post && !isFetching &&
-        <>
-          <div className="d-flex flex-row justify-content-between views mt-5">
-            <span>Dodano datuma: {new Date(post.createdAt).toLocaleDateString()}</span>
-            <span>Pregleda: {post.views}</span>
+  <>
+    <Head>
+      <title>{post.title}</title>
+      <meta name="description" content={post.title} />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="og:title" property="og:title" content="Graph title" />
+      <meta name="robots" content="index, follow" />
+    </Head>
+    <div className="d-flex flex-column flex-wrap single-post">
+      <img src="https://d3sgyrafn929g0.cloudfront.net/wp-content/uploads/sites/18/2016/05/27100558/cover-5-600x700.jpg" className="img-fluid" />
+      <Container>
+        {isFetching &&
+          <div className="d-flex flex-row flex-wrap justify-content-center">
+            <CustomLoader />
           </div>
-          <h1 className="mt-5">{post.title}</h1>
-          <div className="content" dangerouslySetInnerHTML={{ __html: post.content }} />
-        </>
-      }
-    </Container>
-  </div>
+        }
+        {post && !isFetching &&
+          <>
+            <div className="d-flex flex-row justify-content-between views mt-5">
+              <span>Dodano datuma: {new Date(post.createdAt).toLocaleDateString()}</span>
+              <span>Pregleda: {post.views}</span>
+            </div>
+            <h1 className="mt-5">{post.title}</h1>
+            <div className="content" dangerouslySetInnerHTML={{ __html: post.content }} />
+          </>
+        }
+      </Container>
+    </div>
+  </>
 )
 
 export default PostComponent;
