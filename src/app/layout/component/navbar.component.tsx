fbcc11c29cd73ee69107d10d813600c5397e4aee
Mensur <m.bektic@appdevelopment.de>
2019-12-26 11:54:40 +0100

    Changed navbar color on scroll.

--- src/app/layout/component/navbar.component.tsx
+++ src/app/layout/component/navbar.component.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Navbar,
   Nav,
@@ -13,34 +13,51 @@ import {
 import '../styles/navbar.styles.scss';
 
 
-const NavbarComponent = () => (
-  <Navbar light expand="md" className="d-flex flex-row flex-wrap justify-content-center custom-navbar pt-4 pb-4" fixed="top">
-    <Nav className="" navbar>
-      <NavItem>
-        <NavLink href="/components/">Components</NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-      </NavItem>
-      <UncontrolledDropdown nav inNavbar>
-        <DropdownToggle nav caret>
-          Options
+const NavbarComponent = () => {
+  const [scroll, setScroll] = useState(false)
+  const navBackgroundColor = !scroll ? 'transparent' : 'black';
+
+  useEffect(() => {
+    document.addEventListener('scroll', () => {
+      if (window.scrollY > 500 && !scroll) {
+        setScroll(true)
+      }
+      if (window.scrollY < 500 && scroll) {
+        setScroll(false)
+      }
+    })
+  }, [scroll]);
+
+
+  return (
+    <Navbar light expand="md" className="d-flex flex-row flex-wrap justify-content-center custom-navbar pt-4 pb-4" fixed="top" style={{ backgroundColor: navBackgroundColor }}>
+      <Nav className="" navbar>
+        <NavItem>
+          <NavLink href="/components/">Components</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+        </NavItem>
+        <UncontrolledDropdown nav inNavbar>
+          <DropdownToggle nav caret>
+            Options
               </DropdownToggle>
-        <DropdownMenu right>
-          <DropdownItem>
-            Option 1
+          <DropdownMenu right>
+            <DropdownItem>
+              Option 1
                 </DropdownItem>
-          <DropdownItem>
-            Option 2
+            <DropdownItem>
+              Option 2
                 </DropdownItem>
-          <DropdownItem divider />
-          <DropdownItem>
-            Reset
+            <DropdownItem divider />
+            <DropdownItem>
+              Reset
                 </DropdownItem>
-        </DropdownMenu>
-      </UncontrolledDropdown>
-    </Nav>
-  </Navbar>
-)
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      </Nav>
+    </Navbar>
+  )
+}
 
 export default NavbarComponent;
